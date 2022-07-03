896d84e1a057ee33fb8e8b17d5b7369ee2fcb7e1
7heDoer <ifeanyichukwuhillary2019@gmail.com>
2022-07-04 00:57:23 +0100

    Crew page done for relevant screens

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import App from './App'
 import Home from './pages/Home'
+import Destination from './pages/Destination'
+import Crew from './pages/Crew'
+import Technology from './pages/Technology'
 import './styles/index.css'
-
-const Destination = lazy(() => import('./pages/Destination'))
-const Crew = lazy(() => import('./pages/Crew'))
-const Technology = lazy(() => import('./pages/Technology'))
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,9 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<App />} >
           <Route index element={<Home/>}/>
-          <Route path="destinations" element={<Suspense><Destination/></Suspense>}/>
-          <Route path="crew" element={<Suspense><Crew/></Suspense>}/>
-          <Route path="technology" element={<Suspense><Technology/></Suspense>} />
+          <Route path="destinations" element={<Destination/>}/>
+          <Route path="crew" element={<Crew/>}/>
+          <Route path="technology" element={<Technology/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,32 +1,339 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import mobile from '/assets/crew/background-crew-mobile.jpg'
 import tablet from '/assets/crew/background-crew-tablet.jpg'
 import desktop from '/assets/crew/background-crew-desktop.jpg'
+import PageContent from '../util/util'
+
 
 const Section = styled.section`
-    background-image: url(${mobile});
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-    min-block-size: 100vh;
+  background-image: url(${mobile});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  min-block-size: 100vh;
+  padding-block-start: 88px;
+  text-align: center;
+  position: relative;
+
+  .page_heading {
+    font-size: 1rem;
+    font-family: "Barlow Condensed", sans-serif;
+    text-transform: uppercase;
+    letter-spacing: 2.7px;
+    line-height: 19.2px;
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    color: #fff;
+    margin-block-end: 32px;
 
     @media (min-width: 768px) {
-        background-image: url(${tablet});
+      justify-content: start;
+      margin-inline-start: 38.5px;
+      font-size: 20px;
+      line-height: 24px;
+      letter-spacing: 3.8px;
+      margin-block-end: 60px;
     }
 
     @media (min-width: 1024px) {
-        background-image: url(${desktop});
+      position: absolute;
+      margin-inline-start: 100px;
     }
+
+    @media (min-width: 1440px) {
+      margin-inline-start: 166.5px;
+    }
+
+    .page_no {
+      font-weight: 700;
+      opacity: .25;
+      mix-blend-mode: normal;
+    }
+    .title {
+      font-weight: 400;
+    }
+  }
+
+  @media (min-width: 768px) {
+    background-image: url(${tablet});
+    padding-block-start: 136px;
+  }
+
+  @media (min-width: 1024px) {
+    background-image: url(${desktop});
+    padding-block-start: 212px;
+  }
+
+`
+const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    gap: 40px;
+  }
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+  }
+
 
 `
 
-const Destination = () => {
+const Article = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  order: 2;
+  padding-block-start: 32px;
+  border-block-start: 1px solid #383B4B;
+  margin-inline: 24px;
+
+  @media (min-width: 768px) {
+    border: none;
+    order: 1;
+  }
+
+  @media (min-width: 1024px) {
+    flex: 1;
+    margin: 0;
+    margin-block-start: 154px;
+    align-items: flex-start;
+    text-align: start;
+    margin-inline-start: 100px;
+    margin-block-end: 2rem;
+  }
+
+  @media (min-width: 1440px) {
+    margin-inline-start: 165px;
+  }
+
+  .article {
+    order: 2;
+    font-family: "Bellefair", sans-serif;
+    font-weight: 400;
+    color: #fff;
+
+
+    .crew_rank {
+      font-size: 1rem;
+      opacity: .5;
+      mix-blend-mode: normal;
+      text-transform: uppercase;
+      line-height: 18px;
+      margin-block-end: 8px;
+
+      @media (min-width: 768px) {
+        font-size: 24px;
+        line-height: 27.5px;
+      }
+
+      @media (min-width: 1024px) {
+        /* font-size: 24px; */
+        margin-block-end: 15px;
+        /* line-height: 27.5px; */
+      }
+
+      @media (min-width: 1440px) {
+        font-size: 32px;
+        line-height: 36.67px;
+      }
+      
+    }
+
+    .crew_name {
+      font-size: 24px;
+      line-height: 28px;
+      text-transform: uppercase;
+      margin-block-end: 1rem;
+
+      @media (min-width: 768px) {
+        font-size: 40px;
+        line-height: 45.84px;
+      }
+
+      @media (min-width: 1024px) {
+        /* font-size: 24px;
+        line-height: 27.5px; */
+        margin-block-end: 27px;
+      }
+
+      @media (min-width: 1440px) {
+        font-size: 56px;
+        line-height: 64.18px;
+      }
+    }
+
+    .paragraph {
+      font-family: "Barlow", sans-serif;
+      font-size: 15px;
+      line-height: 25px;
+      margin-block-end: 79px;
+
+      @media (min-width: 768px) {
+        font-size: 1rem;
+        max-inline-size: 458px;
+        margin: 0 auto;
+        margin-block-end: 40px;
+      }
+
+      @media (min-width: 1024px) {
+        font-size: 18px;
+        line-height: 32px;
+        max-inline-size: 444px;
+        margin-inline: 0;
+        margin-block-end: 60px;
+      }
+
+      @media (min-width: 1440px) {
+        margin-block-end: 120px;
+      }
+
+
+    }
+  }
+
+`
+
+const Figure = styled.figure`
+  width: 327px;
+  height: 223px;
+  width: auto;
+  margin: 0 auto;
+  order: 1;
+
+  @media (min-width: 768px) {
+    order: 2;
+    width: 100%;
+    height: auto;
+  }
+
+  @media (min-width: 1024px) {
+    flex: 1;
+  }
+
+  @media (min-width: 1440px) {
+    min-inline-size: 453.44px;
+  }
+
+  img {
+    inline-size: auto;
+    block-size: 222px;
+
+    @media (min-width: 768px) {
+      block-size: 572px;
+    }
+  }
+`
+
+const Toggle = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  order: 1;
+  margin-block-end: 32px;
+
+  @media (min-width: 768px) {
+    order: 2;
+    margin-block-end: 0;
+  }
+`
+
+const Btn = styled.button`
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  background-color: #fff;
+  mix-blend-mode: normal;
+  opacity: 0.17;
+  border: none;
+  cursor: pointer;
+
+  &.active {
+    opacity: 1;
+  }
+
+  &.active:hover {
+    opacity: 1;
+  }
+
+  :hover {
+    opacity: 0.5;
+  }
+
+  @media (min-width: 1024px) {
+    width: 15px;
+    height: 15px;
+  }
+`
+
+const Crew = () => {
+
+  const crew = PageContent("crew");
+
+  const [crewMember, setCrewMember] = useState(crew[0])
+
+  const handleState = (n) => {
+    setCrewMember(crew[n]);
+  }
+
+  const onClick = (e, n) => {
+    let target = e.target;
+    handleState(n)
+    let current = document.querySelector('button.active')
+    current.classList.remove('active')
+    target.classList.add('active')
+  }
+
   return (
     <Section aria-label='main content section crew page' className='main_content_section section crew'>
-        Crew
+      <div className="crew_container">
+        <h1 className="page_heading">
+          <span className="page_no">
+            02
+          </span>
+          <span className="title">
+            meet your crew
+          </span>
+        </h1>
+
+        <Content>
+
+          <Article>
+
+            <article className="article">
+              <h2 className="crew_rank">
+                {crewMember.role}
+              </h2>
+              <h3 className="crew_name">
+                {crewMember.name}
+              </h3>
+
+              <p className="paragraph">
+                {crewMember.bio}
+              </p>
+            </article>
+
+            <Toggle>
+              <Btn onClick={(e) => onClick(e, 0)} className='active'></Btn>
+              <Btn onClick={(e) => onClick(e, 1)}></Btn>
+              <Btn onClick={(e) => onClick(e, 2)}></Btn>
+              <Btn onClick={(e) => onClick(e, 3)}></Btn>
+            </Toggle>
+          </Article>
+
+
+          <Figure>
+            <img src={crewMember.images.webp} alt=''/>
+          </Figure>
+        </Content>
+
+
+      </div>
     </Section>
   )
 }
 
-export default Destination
+export default Crew
